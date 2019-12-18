ebf9b9cda2f49681ab25a5a5c3f97bad0d46c03d
Cyril-Xd <54032334+Cyril-Xd@users.noreply.github.com>
2019-12-18 11:07:28 -0800

    Add files via upload

---
+++ jspm_packages/npm/snes9x-next@1.0.0.js
@@ -0,0 +1,3 @@
+(function(System, SystemJS) {(function(require, exports, module, __filename, __dirname, global, GLOBAL) {module.exports = require("npm:snes9x-next@1.0.0/retro.js");
+}).apply(__cjsWrapper.exports, __cjsWrapper.args);
+})(System, System);
