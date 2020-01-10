858392751f45b1f54f331ca9d8141bc7c7ba997f
Cyril-Xd <54032334+Cyril-Xd@users.noreply.github.com>
2020-01-10 09:07:26 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,111 @@
-'use strict';
-/**
- * Cyril-xd offline service worker
- *
- * Based on https://googlechrome.github.io/samples/service-worker/custom-offline-page/index.html.
- */
+/*
+Copyright 2015, 2019 Google Inc. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
 
-// Incrementing CACHE_VERSION will kick off the install event and force
-// previously cached resources to be cached again.
-const CACHE_VERSION = 2;
-const CURRENT_CACHES = {
-  offline: `offline-v${CACHE_VERSION}`,
-};
-const OFFLINE_URL = './index.html';
+// Incrementing OFFLINE_VERSION will kick off the install event and force
+// previously cached resources to be updated from the network.
+const OFFLINE_VERSION = 1;
+const CACHE_NAME = 'offline';
+// Customize this with a different URL if needed.
+const OFFLINE_URL = 'index.html';
 
-function createCacheBustedRequest(url) {
-  const request = new Request(url, { cache: 'reload' });
-  // See https://fetch.spec.whatwg.org/#concept-request-mode
-  // This is not yet supported in Chrome as of M48, so we need to explicitly
-  // check to see if the cache: 'reload' option had any effect.
-  if ('cache' in request) {
-    return request;
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(
+        [
+          './',
+          '/icons/512.png',
+          '/jspm_packages/system.js',
+          '/jspm_packages/system.src.js',
+          '/jspm_packages/npm/vba-next@1.0.0.js',
+          '/jspm_packages/npm/snes9x-next@1.0.0.js',
+          '/jspm_packages/npm/process@0.11.10.js',
+          '/jspm_packages/npm/localforage@1.5.3.js',
+          '/jspm_packages/npm/gambatte@1.0.0.js',
+          '/jspm_packages/npm/vba-next@1.0.0/core.js',
+          '/jspm_packages/npm/vba-next@1.0.0/retro.js',
+          '/jspm_packages/npm/snes9x-next@1.0.0/core.js',
+          '/jspm_packages/npm/snes9x-next@1.0.0/retro.js',
+          '/jspm_packages/npm/gambatte@1.0.0/core.js',
+          '/jspm_packages/npm/gambatte@1.0.0/retro.js',
+          '/build.js',
+          '/config.js',
+          '/index.coffee',
+          '/index.css',
+          '/settings.json',
+          '/utils.js'
+        ]
+      );
+    })
+  );
+});
+
+self.addEventListener('install', (event) => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    // Setting {cache: 'reload'} in the new request will ensure that the response
+    // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
+    await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
+  })());
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    // Enable navigation preload if it's supported.
+    // See https://developers.google.com/web/updates/2017/02/navigation-preload
+    if ('navigationPreload' in self.registration) {
+      await self.registration.navigationPreload.enable();
+    }
+  })());
+
+  // Tell the active service worker to take control of the page immediately.
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // We only want to call event.respondWith() if this is a navigation request
+  // for an HTML page.
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        // First, try to use the navigation preload response if it's supported.
+        const preloadResponse = await event.preloadResponse;
+        if (preloadResponse) {
+          return preloadResponse;
+        }
+
+        const networkResponse = await fetch(event.request);
+        return networkResponse;
+      } catch (error) {
+        // catch is only triggered if an exception is thrown, which is likely
+        // due to a network error.
+        // If fetch() returns a valid HTTP response with a response code in
+        // the 4xx or 5xx range, the catch() will NOT be called.
+        console.log('Fetch failed; returning offline page instead.', error);
+
+        const cache = await caches.open(CACHE_NAME);
+        const cachedResponse = await cache.match(OFFLINE_URL);
+        return cachedResponse;
+      }
+    })());
   }
 
-  // If {cache: 'reload'} didn't have any effect, append a cache-busting URL
-  // parameter instead.
-  const bustedUrl = new URL(url, self.location.href);
-  bustedUrl.search += `${bustedUrl.search ? '&' : ''}cachebust=${Date.now()}`;
-  return new Request(bustedUrl);
-}
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    // We can't use cache.add() here, since we want OFFLINE_URL to be the cache
-    // key, but the actual URL we end up requesting might include a
-    // cache-busting parameter.
-    fetch(createCacheBustedRequest(OFFLINE_URL)).then(response => {
-      return caches.open(CURRENT_CACHES.offline).then(cache => {
-        return cache.put(OFFLINE_URL, response);
-      });
+  // If our if() condition is false, then this fetch handler won't intercept the
+  // request. If there are any other fetch handlers registered, they will get a
+  // chance to call event.respondWith(). If no fetch handlers call
+  // event.respondWith(), the request will be handled by the browser as if there
+  // were no service worker involvement.
+});
     }),
   );
 });
